8c73e6fa5a367f7b9101eae7bc8f5d33e7343f24
nelxaz <nelsonx.zambrano@gmail.com>
2017-09-16 19:07:42 -0400

    Crea componente Navegador

---
+++ src/components/Navegador.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-toolbar class="indigo white--text">
+    <v-toolbar-items>
+      <v-btn flat class="white--text" to="/done" active-class="active" exact>Done ahora!</v-btn>
+    </v-toolbar-items>
+    <v-spacer></v-spacer>
+    <v-text-field
+        append-icon="search"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+    <v-toolbar-items class="hidden-sm-and-down">
+      <v-btn flat class="white--text" to="/registro" active-class="active">Registrarse</v-btn>
+      <v-btn flat class="white--text" to="/ingresar" active-class="active">Ingresar</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
